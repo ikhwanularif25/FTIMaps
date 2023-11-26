e0f9532ccda1d87573443b024a5af6fe5c26461e
ikhwanularif25 <lkhwanularif25@gmail.com>
2023-11-26 19:50:05 +0700

    nambahin page baru

--- description.php
+++ description.php
@@ -12,7 +12,6 @@ if ($conn->connect_error) {
 }
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -20,27 +19,33 @@ if ($conn->connect_error) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/description.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <title>FTIMaps</title>
 </head>
 
 <body>
     <header>
-        <img src="pictures/logoUnand.png" alt="">
+        <div class="logo">
+            <img src="pictures/logoUnand.png" alt="">
         <!-- <img src="pictures/logo2.png" alt=""> -->
+        <a href="index.php"><h1>FTI MINI MAPS</h1></a>
+        
+        </div>
+        
+
+        <div class="search-bar">
+                    <input type="text" placeholder="Search for a location...">
+                    <button type="submit"><i class="fa fa-search"></i></button>
+        </div>
     </header>
 
     <main>
 
         <section class="map-container">
             <div class="deskripsi">
-                <div class="header-deh">
-                    <div class="1">
+                <div class="judul">
                         <h1>FTI MINI</h1>
-                    </div>
-                    <div class="2">
-                        <h1>MAPS</h1>
-                    </div>
                 </div>
 
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos eveniet odit magnam commodi ea nemo
@@ -51,12 +56,9 @@ if ($conn->connect_error) {
                     aliquam placeat, minima, consequatur perferendis labore quae expedita nobis totam odit magni ipsa
                     dolor amet nihil atque. Doloremque eaque ut consequatur..</p>
             </div>
-            <div class="search-maps">
-                <div class="search-bar">
-                    <input type="text" placeholder="Search for a location...">
-                    <button type="submit"><i class="fa fa-search"></i></button>
-                </div>
-                <div class="mini-map">
+            <div class="search-maps2">
+                
+                <div class="mini-map2">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.3092966718223!2d100.45558247590172!3d-0.9152393353350885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2fd4b7969c6d51df%3A0xe826014ce459af90!2sAndalas%20University!5e0!3m2!1sen!2sid!4v1700902816296!5m2!1sen!2sid"
                         width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"
